a4ae594589e3c033b3f895005521e8647dee0491
Osbelkz <43115875+Osbelkz@users.noreply.github.com>
2020-07-02 18:48:03 +0600

    Updates

---
+++ precache-manifest.dd63fbf0b2e32192d5b0e0695b4782a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b198a530bcd5b91211786d8735cd2e4",
+    "url": "/sn.test/index.html"
+  },
+  {
+    "revision": "6ef9c970bcd622c27e61",
+    "url": "/sn.test/static/css/main.df56272b.chunk.css"
+  },
+  {
+    "revision": "ec17f7179be60e4921e5",
+    "url": "/sn.test/static/js/2.f8b2314c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/sn.test/static/js/2.f8b2314c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ef9c970bcd622c27e61",
+    "url": "/sn.test/static/js/main.065d24c8.chunk.js"
+  },
+  {
+    "revision": "ea96aeadd471fa4274b6",
+    "url": "/sn.test/static/js/runtime-main.b53655d8.js"
+  },
+  {
+    "revision": "ea654a43b5ade110628d7aa78c8ea8a5",
+    "url": "/sn.test/static/media/logo.ea654a43.png"
+  }
+]);

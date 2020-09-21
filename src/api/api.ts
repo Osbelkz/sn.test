20a3659ea7b9c7b5857135b435a8d04da286d62c
Askar <osbelkz@gmail.com>
2020-09-21 21:19:14 +0600

    status api

--- src/api/api.ts
+++ src/api/api.ts
@@ -32,6 +32,12 @@ export const profileAPI = {
     getProfile(userId: string) {
         return instance.get(`profile/${userId}`)
             .then(res => res.data)
+    },
+    getStatus(userId: string) {
+        return instance.get(`profile/status/${userId}`)
+    },
+    updateStatus(status: string) {
+        return instance.put(`profile/status`, {status})
     }
 }
 

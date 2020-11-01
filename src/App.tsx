af381d8d1116320a961c6543a77870908836c79d
Askar <osbelkz@gmail.com>
2020-11-01 11:40:21 +0600

    refactor: React lazy and suspense

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,8 @@ import {Route, Switch, withRouter} from 'react-router-dom';
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
-import DialogsContainer from "./components/Dialogs/DialogsContainer";
+
 import UsersContainer from "./components/Users/UsersContainer";
-import ProfileContainer from "./components/Profile/ProfileContainer";
 import LoginPage from "./components/Login/Login";
 import NavContainer from "./components/Nav/NavContainer";
 import {connect} from "react-redux";
@@ -14,6 +13,10 @@ import {compose} from "redux";
 import {initializeAppTC} from "./redux/reducers/actions/app-actions";
 import {RootStateType} from "./redux/redux-store";
 import {Preloader} from "./components/UI/Preloader/Preloader";
+
+
+const DialogsContainer = React.lazy(() => import("./components/Dialogs/DialogsContainer"))
+const ProfileContainer = React.lazy(() => import("./components/Profile/ProfileContainer"))
 
 type MapDispatchToPropsType = {
     initializeAppTC: () => void
@@ -33,7 +36,7 @@ class App extends React.Component<AppPropsTypes> {
 
     render() {
         if (!this.props.initialized) {
-            return <Preloader />
+            return <Preloader/>
         }
 
         return (
@@ -41,14 +44,24 @@ class App extends React.Component<AppPropsTypes> {
                 <NavContainer/>
                 <div className='app-wrapper-content'>
                     <Switch>
-                        <Route path='/profile/:userId?' component={ProfileContainer}/>
-                        <Route path='/messages' component={DialogsContainer}/>
+                        <Route path='/profile/:userId?' render={() =>
+                            <React.Suspense fallback={<Preloader/>}>
+                                <ProfileContainer/>
+                            </React.Suspense>
+                        }/>
+                        <Route path='/messages' render={() =>
+                            <React.Suspense fallback={<Preloader/>}>
+                                <DialogsContainer/>
+                            </React.Suspense>}/>
                         <Route path='/users' component={UsersContainer}/>
                         <Route path='/news' component={News}/>
                         <Route path='/music' component={Music}/>
                         <Route path='/settings' component={Settings}/>
                         <Route path='/login' component={LoginPage}/>
-                        <Route path='/' component={ProfileContainer}/>
+                        <Route path='/' render={() =>
+                            <React.Suspense fallback={<Preloader/>}>
+                                <ProfileContainer/>
+                            </React.Suspense>}/>
                     </Switch>
                 </div>
             </div>
@@ -64,5 +77,5 @@ const mapStateToProps = (state: RootStateType): MapStateToPropsType => ({
 export default compose<any>(
     withRouter,
     connect(mapStateToProps, {initializeAppTC})
-    )(App)
+)(App)
 ;
--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -1,4 +1,4 @@
-import {applyMiddleware, combineReducers, createStore} from "redux";
+import {combineReducers} from "redux";
 import {profileReducer} from "./reducers/profile-page-reducer";
 import {dialogsReducer} from "./reducers/dialogs-reducer";
 import {usersReducer} from "./reducers/users-reducer";
@@ -6,8 +6,9 @@ import {authReducer} from "./reducers/auth-reducer";
 import thunkMiddleware from "redux-thunk"
 import {reducer as formReducer} from "redux-form";
 import {appReducer} from "./reducers/app-reducer";
+import {configureStore} from "@reduxjs/toolkit";
 
-let reducers = combineReducers({
+let rootReducer = combineReducers({
     profilePage: profileReducer,
     dialogsPage: dialogsReducer,
     usersPage: usersReducer,
@@ -16,9 +17,12 @@ let reducers = combineReducers({
     app: appReducer,
 })
 
-export type RootStateType = ReturnType<typeof reducers>
+export type RootStateType = ReturnType<typeof rootReducer>
 
-let store = createStore(reducers, applyMiddleware(thunkMiddleware));
+let store = configureStore({
+    reducer: rootReducer,
+    middleware: getDefaultMiddleware => getDefaultMiddleware().prepend(thunkMiddleware)
+});
 
 export default store;
 
